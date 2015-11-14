b3387fc94ae5d4c94b9bcef92e87872690682c57
Kashish Gupta <kashish.gupta@intelligrape.com>
2015-11-14 14:39:28 +0530

    final commit

--- app.js
+++ app.js
@@ -3,35 +3,43 @@ var gmail = require('./gmailSend');
 
 gmail(function (email) {
 
-    /*email.send({
-        from: 'guptkashish@gmail.com',
-        to: 'guptkashish@gmail.com',
-        subject: 'Internet Speed Notification - ' + new Date(),
-        body: 'I am body',
-        html: '<b>YOYO</b>'
-    }, function (err, data) {
-        if (err) {
-            console.log(err);
-        } else {
-            console.log('Email Send Successfully at ' + new Date());
-            console.log(data);
-        }
-    });*/
-
-
-
     test=speedTest({maxTime:5000});
 
-    test.on('data',function(data){
-        console.dir(data);
+    test.on('data',function(speedResult){
+
+        email.send({
+            from: 'guptkashish@gmail.com',
+            to: 'guptkashish@gmail.com',
+            subject: 'Internet Speed Notification - ' + new Date(),
+            body: JSON.stringify(speedResult)
+        }, function (err, data) {
+            if (err) {
+                console.log(err);
+            } else {
+                console.log('Email Send Successfully at ' + new Date());
+            }
+        });
+
     });
 
     test.on('error',function(err){
-        console.error(err);
+        email.send({
+            from: 'guptkashish@gmail.com',
+            to: 'guptkashish@gmail.com',
+            subject: 'Internet Speed Notification - ' + new Date(),
+            body: JSON.stringify(err)
+        }, function (err, data) {
+            if (err) {
+                console.log(err);
+            } else {
+                console.log('Error Report Send Successfully at ' + new Date());
+            }
+        });
     });
 
-
 });
+
+
 
 
 
