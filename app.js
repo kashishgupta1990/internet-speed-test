d487685112b1cdbbd94a4df1a65558e7105d71d8
Kashish Gupta <kashish.gupta@intelligrape.com>
2015-11-14 13:00:33 +0530

    init

---
+++ app.js
@@ -0,0 +1,12 @@
+//var speedTest=require('speedtest-net');
+//test=speedTest();
+
+/*
+test.on('data',function(data){
+    console.log('Data ', data);
+});
+
+test.on('error',function(err){
+    console.error(err);
+});*/
+
