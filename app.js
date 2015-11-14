e36d9792e07c07e36299763d98458943c99abb82
Kashish Gupta <kashish.gupta@intelligrape.com>
2015-11-14 13:50:28 +0530

    testing speed test tool

--- app.js
+++ app.js
@@ -1,12 +1,38 @@
-//var speedTest=require('speedtest-net');
-//test=speedTest();
+var speedTest=require('speedtest-net');
+var gmail = require('./gmailSend');
 
-/*
-test.on('data',function(data){
-    console.log('Data ', data);
+gmail(function (email) {
+
+    /*email.send({
+        from: 'guptkashish@gmail.com',
+        to: 'guptkashish@gmail.com',
+        subject: 'Internet Speed Notification - ' + new Date(),
+        body: 'I am body',
+        html: '<b>YOYO</b>'
+    }, function (err, data) {
+        if (err) {
+            console.log(err);
+        } else {
+            console.log('Email Send Successfully at ' + new Date());
+            console.log(data);
+        }
+    });*/
+
+
+
+    test=speedTest({maxTime:5000});
+
+    test.on('data',function(data){
+        console.dir(data);
+    });
+
+    test.on('error',function(err){
+        console.error(err);
+    });
+
+
 });
 
-test.on('error',function(err){
-    console.error(err);
-});*/
+
+
 
